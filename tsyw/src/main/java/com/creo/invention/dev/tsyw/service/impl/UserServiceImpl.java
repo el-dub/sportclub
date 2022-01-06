43620450337189d338416316381a249b39e3e0c7
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2022-01-06 23:20:46 +0200

    implemented generating jwt tokens

--- tsyw/src/main/java/com/creo/invention/dev/tsyw/service/impl/UserServiceImpl.java
+++ tsyw/src/main/java/com/creo/invention/dev/tsyw/service/impl/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.creo.invention.dev.tsyw.service.impl;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.creo.invention.dev.tsyw.dto.mapper.UserMapper;
 import com.creo.invention.dev.tsyw.dto.user.CreateUserDto;
 import com.creo.invention.dev.tsyw.model.Role;
@@ -8,6 +10,7 @@ import com.creo.invention.dev.tsyw.repository.RoleRepository;
 import com.creo.invention.dev.tsyw.repository.UserRepository;
 import com.creo.invention.dev.tsyw.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +23,9 @@ public class UserServiceImpl implements UserService {
     private final UserRepository repository;
     private final RoleRepository roleRepository;
 
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
+
     @Override
     public String createUser(CreateUserDto dto) {
         var user = userMapper.fromCreateDto(dto);
@@ -28,11 +34,20 @@ public class UserServiceImpl implements UserService {
 
         user = repository.save(user);
 
+        // TODO: remove this kostyl
+        var role = new Role();
+        role.setRoleId(1);
+        role.setRoleName("user");
+        user.setRole(role);
+
         return generateJwtForUser(user);
     }
 
     private String generateJwtForUser(User user) {
-        return "todo";
+        return JWT.create()
+                .withClaim("uuid", user.getUserId().toString())
+                .withClaim("role", user.getRole().getRoleName())
+                .sign(Algorithm.HMAC256(JWT_SECRET.getBytes()));
     }
 
 }

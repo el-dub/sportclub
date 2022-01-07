d7ef569d41b3ebf0eab04659c3ba2bd46092e979
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2022-01-08 01:38:18 +0200

    added something  i don't remember what

--- tsyw/src/main/java/com/creo/invention/dev/tsyw/controller/UserSubscriptionController.java
+++ tsyw/src/main/java/com/creo/invention/dev/tsyw/controller/UserSubscriptionController.java
@@ -1,6 +1,11 @@
 package com.creo.invention.dev.tsyw.controller;
 
+import com.creo.invention.dev.tsyw.exception.WrongCredentialsException;
+import com.creo.invention.dev.tsyw.service.UserService;
+import com.creo.invention.dev.tsyw.service.UserSubscriptionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user-subscriptions")
 @RequiredArgsConstructor
 public class UserSubscriptionController {
+
+    private UserSubscriptionService service;
+    private UserService userService;
+
+    @GetMapping
+    String getUserSubscriptions(@RequestHeader("authorization") String token) {
+        try {
+            var user = userService.getUserFromToken(token);
+            //
+            return "todo";
+        } catch (WrongCredentialsException e) {
+            return "{\"error\": 1}";
+        }
+    }
+
+
 }
---
+++ tsyw/src/main/java/com/creo/invention/dev/tsyw/exception/WrongCredentialsException.java
@@ -0,0 +1,9 @@
+package com.creo.invention.dev.tsyw.exception;
+
+public class WrongCredentialsException extends Exception {
+
+    public WrongCredentialsException(String message) {
+        super(message);
+    }
+
+}
